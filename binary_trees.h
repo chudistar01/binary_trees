ae5364f18eeb48057ebfc094c19e4fb11880b1b8
chudistar01 <christianigwebuike31@gmail.com>
2024-04-12 18:25:32 -0700

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,4 @@
-#ifndef _BINARY_TREES_H_
+i#ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
@@ -45,4 +45,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif
